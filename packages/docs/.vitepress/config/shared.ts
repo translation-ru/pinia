03f72437f6f53b17e3e8561efcb898b6188b3471
Nikita Perfilyev <leronez.dev@gmail.com>
2023-10-10 11:05:24 +0900

    search: configure algolia

--- packages/docs/.vitepress/config/shared.ts
+++ packages/docs/.vitepress/config/shared.ts
@@ -132,9 +132,49 @@ export const sharedConfig = defineConfig({
     },
 
     algolia: {
-      appId: '69Y3N7LHI2',
-      apiKey: '45441f4b65a2f80329fd45c7cb371fea',
-      indexName: 'pinia',
+      appId: '7I08VA6IQF',
+      apiKey: '697c26a2f6a3f0308a58b138dc4269a6',
+      indexName: 'pinia-ru',
+      placeholder: 'Поиск по документации',
+      translations: {
+        button: {
+          buttonText: 'Поиск',
+          buttonAriaLabel: 'Поиск',
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: 'Сбросить запрос',
+            resetButtonAriaLabel: 'Сбросить запрос',
+            cancelButtonText: 'Отмена',
+            cancelButtonAriaLabel: 'Отмена',
+          },
+          startScreen: {
+            recentSearchesTitle: 'История поиска',
+            noRecentSearchesText: 'Нет истории поиска',
+            saveRecentSearchButtonTitle: 'Добавить в избранное',
+            removeRecentSearchButtonTitle: 'Удалить из истории поиска',
+            favoriteSearchesTitle: 'Избранное',
+            removeFavoriteSearchButtonTitle: 'Удалить из избранного',
+          },
+          errorScreen: {
+            titleText: 'Невозможно получить результаты',
+            helpText: 'Возможно, вам нужно проверить сетевое соединение',
+          },
+          footer: {
+            selectText: 'выбор',
+            navigateText: 'навигация',
+            closeText: 'закрыть',
+            searchByText: 'Поставщик поиска',
+          },
+          noResultsScreen: {
+            noResultsText: 'Нет результатов по запросу',
+            suggestedQueryText: 'Попробуйте поискать',
+            reportMissingResultsText:
+              'Как вы считаете, должен ли этот запрос возвращать результаты?',
+            reportMissingResultsLinkText: 'Сообщите нам об этом.',
+          },
+        },
+      },
     },
 
     carbonAds: {
