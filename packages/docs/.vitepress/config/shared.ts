7be0c95a53547171786c9bb10c9f91d7fe91f927
Nikita Perfilyev <leronez.dev@gmail.com>
2023-10-18 11:04:45 +0900

    docs(ru): fix localization of search

--- packages/docs/.vitepress/config/shared.ts
+++ packages/docs/.vitepress/config/shared.ts
@@ -131,47 +131,50 @@ export const sharedConfig = defineConfig({
       text: 'Suggest changes',
     },
 
-    algolia: {
-      appId: '7I08VA6IQF',
-      apiKey: '697c26a2f6a3f0308a58b138dc4269a6',
-      indexName: 'pinia-ru',
-      placeholder: 'Поиск по документации',
-      translations: {
-        button: {
-          buttonText: 'Поиск',
-          buttonAriaLabel: 'Поиск',
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: 'Сбросить запрос',
-            resetButtonAriaLabel: 'Сбросить запрос',
-            cancelButtonText: 'Отмена',
-            cancelButtonAriaLabel: 'Отмена',
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: '7I08VA6IQF',
+        apiKey: '697c26a2f6a3f0308a58b138dc4269a6',
+        indexName: 'pinia-ru',
+        placeholder: 'Поиск по документации',
+        translations: {
+          button: {
+            buttonText: 'Поиск',
+            buttonAriaLabel: 'Поиск',
           },
-          startScreen: {
-            recentSearchesTitle: 'История поиска',
-            noRecentSearchesText: 'Нет истории поиска',
-            saveRecentSearchButtonTitle: 'Добавить в избранное',
-            removeRecentSearchButtonTitle: 'Удалить из истории поиска',
-            favoriteSearchesTitle: 'Избранное',
-            removeFavoriteSearchButtonTitle: 'Удалить из избранного',
-          },
-          errorScreen: {
-            titleText: 'Невозможно получить результаты',
-            helpText: 'Возможно, вам нужно проверить сетевое соединение',
-          },
-          footer: {
-            selectText: 'выбор',
-            navigateText: 'навигация',
-            closeText: 'закрыть',
-            searchByText: 'Поставщик поиска',
-          },
-          noResultsScreen: {
-            noResultsText: 'Нет результатов по запросу',
-            suggestedQueryText: 'Попробуйте поискать',
-            reportMissingResultsText:
-              'Как вы считаете, должен ли этот запрос возвращать результаты?',
-            reportMissingResultsLinkText: 'Сообщите нам об этом.',
+          modal: {
+            searchBox: {
+              resetButtonTitle: 'Сбросить запрос',
+              resetButtonAriaLabel: 'Сбросить запрос',
+              cancelButtonText: 'Отмена',
+              cancelButtonAriaLabel: 'Отмена',
+            },
+            startScreen: {
+              recentSearchesTitle: 'История поиска',
+              noRecentSearchesText: 'Нет истории поиска',
+              saveRecentSearchButtonTitle: 'Добавить в избранное',
+              removeRecentSearchButtonTitle: 'Удалить из истории поиска',
+              favoriteSearchesTitle: 'Избранное',
+              removeFavoriteSearchButtonTitle: 'Удалить из избранного',
+            },
+            errorScreen: {
+              titleText: 'Невозможно получить результаты',
+              helpText: 'Возможно, вам нужно проверить сетевое соединение',
+            },
+            footer: {
+              selectText: 'выбор',
+              navigateText: 'навигация',
+              closeText: 'закрыть',
+              searchByText: 'Поставщик поиска',
+            },
+            noResultsScreen: {
+              noResultsText: 'Нет результатов по запросу',
+              suggestedQueryText: 'Попробуйте поискать',
+              reportMissingResultsText:
+                'Как вы считаете, должен ли этот запрос возвращать результаты?',
+              reportMissingResultsLinkText: 'Сообщите нам об этом.',
+            },
           },
         },
       },
