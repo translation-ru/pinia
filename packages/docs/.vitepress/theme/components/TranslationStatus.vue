e9886cba1d46e83ce16f6a99646cfd49f76ed380
Nikita Perfilyev <leronez.dev@gmail.com>
2024-03-18 12:43:12 +0900

    feat(docs): enable TranslationStatus component for ru locale

---
+++ packages/docs/.vitepress/theme/components/TranslationStatus.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+const originalLang = 'en'
+const i18nLabels: {
+  [lang: string]: string
+} = {
+  en: 'The translation is synced to the docs on ${date} of which the commit hash is <code>${hash}</code>.',
+  zh: '该翻译已同步到了 ${date} 的版本，其对应的 commit hash 是 <code>${hash}</code>。',
+  ru: 'Перевод синхронизирован с документацией от ${date}, хэш коммита <code>${hash}</code>.',
+}
+</script>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useData } from 'vitepress'
+import status from '../../translation-status.json'
+
+const { site } = useData()
+const label = computed<string>(() => {
+  const localeIndex = 'ru'
+
+  if (!localeIndex || !status[localeIndex]) {
+    return ''
+  }
+  const { date, hash } = status[localeIndex]
+  return (i18nLabels[localeIndex] || i18nLabels.en)
+    .replace('${date}', `<time>${date}</time>`)
+    .replace('${hash}', `<code>${hash.substring(0, 7)}</code>`)
+})
+</script>
+
+<template>
+  <div v-if="label" class="text-status" v-html="label"></div>
+</template>
+
+<style scoped>
+.text-status {
+  padding: 1em 1.25em;
+  font-size: small;
+  text-align: right;
+  color: var(--vp-c-text-2);
+}
+</style>
